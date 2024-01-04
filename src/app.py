f530de267c516df6f15444089f06c849c0e1cc30
Lena Merkli <lena.merkli@protonmail.com>
2024-01-04 21:20:03 +0100

    Move create tables to additional file

--- src/app.py
+++ src/app.py
@@ -253,28 +253,6 @@ class User:
             'grade': self.grade,
         }
 
-    @staticmethod
-    def create_table() -> None:
-        """
-        Creates the necessary SQL table
-        :return: None
-        """
-        query_db('CREATE TABLE IF NOT EXISTS users ('
-                 'id TEXT PRIMARY KEY, '
-                 'name TEXT NOT NULL, '
-                 'mail TEXT NOT NULL, '
-                 'salt TEXT NOT NULL, '
-                 'hash TEXT NOT NULL, '
-                 'newsletter INTEGER NOT NULL, '
-                 'created TEXT NOT NULL, '
-                 'theme TEXT NOT NULL, '
-                 'iframe INTEGER NOT NULL, '
-                 'payment TEXT NOT NULL, '
-                 'banned TEXT NOT NULL, '
-                 'search TEXT NOT NULL, '
-                 'class TEXT NOT NULL, '
-                 'grade TEXT NOT NULL)')
-
     def save(self) -> None:
         """
         Saves the user in the database.
@@ -531,27 +509,6 @@ class Document:
             'mimetype': self.mimetype,
             'size': self.size,
         }
-
-    @staticmethod
-    def create_table() -> None:
-        """
-        Creates the necessary SQL table
-        :return: None
-        """
-        query_db('CREATE TABLE IF NOT EXISTS documents ('
-                 'id TEXT PRIMARY KEY, '
-                 'title TEXT NOT NULL, '
-                 'subject TEXT NOT NULL, '
-                 'description TEXT NOT NULL, '
-                 'class TEXT NOT NULL, '
-                 'grade TEXT NOT NULL, '
-                 'language TEXT NOT NULL, '
-                 'owner TEXT NOT NULL, '
-                 'edited TEXT NOT NULL, '
-                 'created TEXT NOT NULL, '
-                 'extension TEXT NOT NULL, '
-                 'mimetype TEXT NOT NULL, '
-                 'size INTEGER NOT NULL)')
 
     def save(self) -> None:
         """
